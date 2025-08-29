ca7656103db098fb8f0efb25928a1a843a036d09
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2025-08-29 07:06:47 +0300

    push

---
+++ lib/fluid_card.dart
@@ -0,0 +1,132 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class FluidCard extends StatefulWidget {
+  final String imageColorName;
+  final Color altColor;
+  final String title;
+  final String subtitle;
+
+  const FluidCard({
+    super.key,
+    required this.imageColorName,
+    this.title = "",
+    required this.subtitle,
+    required this.altColor,
+  });
+
+  @override
+  State<FluidCard> createState() => _FluidCardState();
+}
+
+class _FluidCardState extends State<FluidCard> {
+  late Ticker _ticker;
+
+  @override
+  void initState() {
+    _ticker = Ticker((d) {
+      setState(() {});
+    })..start();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (_, constraints) {
+        var time = DateTime.now().millisecondsSinceEpoch / 2000;
+        var scaleX = 1.2 + sin(time) * 5;
+        var scaleY = 1.2 + cos(time) * 7;
+        var offsetY = 20 + cos(time) * 20;
+        final width = constraints.maxWidth;
+        final height = constraints.maxHeight;
+        return Stack(
+          alignment: Alignment.center,
+          fit: StackFit.expand,
+          children: <Widget>[
+            Transform.translate(
+              offset: Offset(-(scaleX - 1) / 2 * width, -(scaleY - 1) / 2 * height + offsetY),
+              child: Transform(
+                transform: Matrix4.diagonal3Values(scaleX, scaleY, 1),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: widget.altColor,
+                    // image: DecorationImage(
+                    //   image: AssetImage('assets/images/Bg-${widget.color}.png'),
+                    //   fit: BoxFit.cover,
+                    // ),
+                  ),
+                ),
+                // child: Image.asset('assets/images/Bg-${widget.color}.png', fit: BoxFit.cover),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 75, bottom: 25),
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Image.asset(
+                          'assets/images/Illustration-${widget.imageColorName}.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 14, child: Image.asset('assets/images/Slider-${widget.imageColorName}.png')),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(padding: const EdgeInsets.symmetric(horizontal: 18), child: _buildBottomContent()),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildBottomContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          widget.title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            height: 1,
+            fontSize: 30,
+            fontFamily: 'MarcellusSC',
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        Text(
+          widget.subtitle,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+            fontFamily: 'Lexend',
+            color: Colors.white.withValues(alpha: 0.7),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'fluid_card.dart';
+import 'fluid_carousel.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent, systemNavigationBarColor: Colors.transparent),
+  );
+
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: FluidCarousel(
+          children: [
+            FluidCard(
+              imageColorName: 'Red',
+              altColor: Color(0xFF4259B2),
+              title: "Start Your Day \nwith Peaceful Mornings",
+              subtitle: "Wake up refreshed with calming nature-inspired sounds designed to ease you into the day.",
+            ),
+            FluidCard(
+              imageColorName: 'Yellow',
+              altColor: Color(0xFF904E93),
+              title: "Refresh Your Mind \nwith Guided Breathing",
+              subtitle: "Reduce stress and boost focus with science-backed breathing techniques at your fingertips.",
+            ),
+            FluidCard(
+              imageColorName: 'Blue',
+              altColor: Color(0xFFFFB138),
+              title: "Sleep Soundly \nwith Soothing Stories",
+              subtitle:
+                  "Drift into deep sleep with a collection of relaxing bedtime stories and mindfulness exercises.",
+            ),
+          ],
+        ),
+      ),
+
+      ///
+    );
+  }
+}
